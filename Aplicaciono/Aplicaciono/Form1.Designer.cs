39106e982b7b7d10f34e0112148038fd5297aa6c
Sara <lethe.agbr@gmail.com>
2019-02-20 20:12:06 +0100

    Primera conexion

--- Aplicaciono/Aplicaciono/Form1.Designer.cs
+++ Aplicaciono/Aplicaciono/Form1.Designer.cs
@@ -34,16 +34,16 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(9, 7);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(129, 23);
+            this.label1.Size = new System.Drawing.Size(167, 28);
             this.label1.TabIndex = 0;
             this.label1.Text = "Personales datos";
             // 
@@ -51,31 +51,19 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(9, 43);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(12, 53);
             this.label2.Name = "label2";
-<<<<<<< Updated upstream
-            this.label2.Size = new System.Drawing.Size(163, 23);
+            this.label2.Size = new System.Drawing.Size(208, 28);
             this.label2.TabIndex = 1;
             this.label2.Text = "Cuantos partos cacho";
-=======
-            this.label2.Size = new System.Drawing.Size(180, 28);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Nombre y apellido";
->>>>>>> Stashed changes
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(9, 66);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(12, 81);
             this.label3.Name = "label3";
-<<<<<<< Updated upstream
-            this.label3.Size = new System.Drawing.Size(132, 23);
-=======
             this.label3.Size = new System.Drawing.Size(53, 28);
->>>>>>> Stashed changes
             this.label3.TabIndex = 2;
             this.label3.Text = "DNI";
             // 
@@ -83,14 +71,9 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(9, 89);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(12, 110);
             this.label4.Name = "label4";
-<<<<<<< Updated upstream
-            this.label4.Size = new System.Drawing.Size(132, 23);
-=======
             this.label4.Size = new System.Drawing.Size(100, 28);
->>>>>>> Stashed changes
             this.label4.TabIndex = 3;
             this.label4.Text = "Dirección";
             // 
@@ -98,14 +81,9 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(9, 111);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Location = new System.Drawing.Point(12, 137);
             this.label5.Name = "label5";
-<<<<<<< Updated upstream
-            this.label5.Size = new System.Drawing.Size(132, 23);
-=======
             this.label5.Size = new System.Drawing.Size(100, 28);
->>>>>>> Stashed changes
             this.label5.TabIndex = 4;
             this.label5.Text = "Municipio";
             // 
@@ -113,29 +91,35 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(9, 134);
-            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Location = new System.Drawing.Point(12, 165);
             this.label6.Name = "label6";
-<<<<<<< Updated upstream
-            this.label6.Size = new System.Drawing.Size(132, 23);
-=======
             this.label6.Size = new System.Drawing.Size(95, 28);
->>>>>>> Stashed changes
             this.label6.TabIndex = 5;
             this.label6.Text = "Provincia";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(443, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 44);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Test";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -151,6 +135,7 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Aplicaciono/Aplicaciono/Form1.cs
+++ Aplicaciono/Aplicaciono/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,31 @@ namespace Aplicaciono
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string infoConexion = "data source=127.0.0.1;initial catalog=AppGestion;user id=root;password=;";
+            SqlConnection con = new SqlConnection(infoConexion);
+            con.Open();
+            if (con.State == ConnectionState.Open)
+            {
+                string q = "Select nombre from Usuario";
+                SqlCommand cmd = new SqlCommand();
+                SqlDataReader reader;
+
+                cmd.CommandText = "SELECT nombre FROM Customers";
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = con;
+                string newID = (string)cmd.ExecuteScalar();
+
+                con.Open();
+
+                reader = cmd.ExecuteReader();
+                // Data is accessible through the DataReader object here.
+
+                con.Close();
+            }
         }
     }
 }
