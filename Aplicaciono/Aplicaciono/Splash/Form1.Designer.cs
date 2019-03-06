b0f55900cba146bcb16c3e80b24e640855e06ded
EugenioGutierrezC <eugeniogutierrez.c@gmail.com>
2019-03-06 20:40:26 +0100

    Form a SplashView + interface splash

---
+++ Aplicaciono/Aplicaciono/Splash/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Aplicaciono
+{
+    partial class SplashView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Cargando = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            // 
+            // Cargando
+            // 
+            this.Cargando.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Cargando.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cargando.Location = new System.Drawing.Point(0, 0);
+            this.Cargando.Name = "Cargando";
+            this.Cargando.Size = new System.Drawing.Size(232, 147);
+            this.Cargando.TabIndex = 0;
+            this.Cargando.Text = "Cargando";
+            this.Cargando.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // SplashView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(232, 147);
+            this.Controls.Add(this.Cargando);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "SplashView";
+            this.Text = "App Gestión";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label Cargando;
+    }
+}
+
