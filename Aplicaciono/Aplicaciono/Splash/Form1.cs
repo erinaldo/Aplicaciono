8385aa713b2faff87fae810e92091c60680ab86a
EugenioGutierrezC <eugeniogutierrez.c@gmail.com>
2019-03-21 20:28:08 +0100

    Splash funcionando bien y cerrandose despues de cerrar

--- Aplicaciono/Aplicaciono/Splash/Form1.cs
+++ Aplicaciono/Aplicaciono/Splash/Form1.cs
@@ -13,6 +13,27 @@ namespace Aplicaciono
 
             Conexione repo = new Conexione();
             SplashInicia splashInicia = new SplashInicia(repo);
-        }  
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Increment(1);
+
+            if (progressBar1.Value == 100)
+            {
+                System.Threading.Thread.Sleep(5000);
+
+                timer1.Stop();
+                this.Visible = false;
+            }
+        }
+
+        protected override void OnVisibleChanged(EventArgs e)
+        {
+            
+                base.OnVisibleChanged(e);
+                //this.Visible = false;
+            
+        }
     }
 }
