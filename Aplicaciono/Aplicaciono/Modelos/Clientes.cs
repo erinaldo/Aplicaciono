bd60826a4eb6069299d76621d1f32323b453d391
EugenioGutierrezC <eugeniogutierrez.c@gmail.com>
2019-06-03 19:50:45 +0200

    Creado modelo v1

---
+++ Aplicaciono/Aplicaciono/Modelos/Clientes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicaciono.Modelos
+{
+    class Clientes
+    {
+        public int id { get; }
+        public string nombre { get; set; }
+        public string precio { get; set; }
+
+        public Clientes(string nombre, string precio)
+        {
+            this.nombre = nombre;
+            this.precio = precio;
+        }
+
+        public Clientes(int id, string nombre, string precio)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.precio = precio;
+        }
+    }
+}
